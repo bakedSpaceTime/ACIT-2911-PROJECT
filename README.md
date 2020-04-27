b6c33f4ccfb5a7c62f6b518fb1144fcf9f758ab3
jeffreylaw <lawjeffrey35@gmail.com>
2020-04-27 16:41:26 -0700

    uploading audio, cleaned code, basic movement, wall collision

--- README.md
+++
@@ -1,2 +0,0 @@
-# ACIT-2911-PROJECT
-Scrum Term Project For ACIT 2911, BCIT Winter Semester 2020
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from game import Game
 
 if __name__ == "__main__":
     game = Game()
+    # game.game_intro()
     game.main_game()
