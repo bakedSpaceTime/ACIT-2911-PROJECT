15bb7511cfa9167f4201bc03a45ea478aefd52d5
bakedSpaceTime <tushyais@gmail.com>
2020-04-27 01:48:22 -0700

    refactor code into game class

--- game.py
+++ game.py
@@ -1,99 +1,96 @@
 import pygame
 
+class Game():
+    def __init__(self):
 
-def redraw():
-    window.fill((0, 0, 0))
-    # window.blit(bg, (0,0))
-    if left:
-        window.blit(charLeft, (x,y))
-    elif right:
-        window.blit(charRight, (x,y))
-    elif up:
-        window.blit(charUp, (x,y))
-    elif down:
-        window.blit(charDown, (x, y))
-    # else:
-    #     window.blit(charDown, (x, y))
-    pygame.display.update()
+        pygame.init()
+        self.window = pygame.display.set_mode((500, 500))
+        self.x = 225
+        self.y = 225
+        self.width = 32
+        self.height = 32
+        self.velocity = 10
+        self.left = False
+        self.right = False
+        self.up = False
+        self.down = False
 
-# def main_game():
-pygame.init()
+        # self.bg = pygame.image.load('images/bg.jpg')
+        self.char = pygame.image.load('images/pac-up.bmp')
+        self.charRight = pygame.image.load('images/pac-right.bmp')
+        self.charLeft = pygame.image.load('images/pac-left.bmp')
+        self.charUp = pygame.image.load('images/pac-up.bmp')
+        self.charDown = pygame.image.load('images/pac-down.bmp')
 
-window = pygame.display.set_mode((500, 500))
+        self.clock = pygame.time.Clock()
+        self.run = True
 
-pygame.display.set_caption("Maze")
+    def redraw(self):
+        self.window.fill((0, 0, 0))
+        # self.window.blit(bg, (0,0))
+        if self.left:
+            self.window.blit(self.charLeft, (self.x,self.y))
+        elif self.right:
+            self.window.blit(self.charRight, (self.x,self.y))
+        elif self.up:
+            self.window.blit(self.charUp, (self.x,self.y))
+        elif self.down:
+            self.window.blit(self.charDown, (self.x, self.y))
+        pygame.display.update()
+        pygame.display.set_caption("Pandemic Run")
 
-bg = pygame.image.load('images/bg.jpg')
-char = pygame.image.load('images/pac-up.bmp')
-charRight = pygame.image.load('images/pac-right.bmp')
-charLeft = pygame.image.load('images/pac-left.bmp')
-charUp = pygame.image.load('images/pac-up.bmp')
-charDown = pygame.image.load('images/pac-down.bmp')
+    def main_game(self):
+        while True:
+            self.clock.tick(30)
+            # pygame.time.delay(50)
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    pygame.quit()
+            keys = pygame.key.get_pressed()
 
-clock = pygame.time.Clock()
-
-x = 225
-y = 225
-width = 32
-height = 32
-velocity = 10
-left = False
-right = False
-up = False
-down = False
-
-
-while True:
-    clock.tick(30)
-    # pygame.time.delay(50)
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            pygame.quit()
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_LEFT]:
-        if x > 0 - velocity:
-            x -= velocity
-            left = True
-            right = False
-            up = False
-            down = False
-    elif keys[pygame.K_RIGHT]:
-        if x < 500 - width - velocity:
-            x += velocity
-            right = True
-            left = False
-            up = False
-            down = False
-    elif keys[pygame.K_UP]:
-        if y > 0:
-            y -= velocity
-            right = False
-            left = False
-            up = True
-            down = False
-    elif keys[pygame.K_DOWN]:
-        if y < 500 - height:
-            y += velocity
-            right = False
-            left = False
-            up = False
-            down = True
-    else:
-        if right:
-            if x < 500 - width - velocity:
-                x += velocity
-        elif left:
-            if x > 0 + velocity:
-                x -= velocity
-        elif up:
-            if y > 0 + velocity:
-                y -= velocity
-        elif down:
-            if y < 500 - height - velocity:
-                y += velocity
-    print(x,y)
-    redraw()
+            if keys[pygame.K_LEFT]:
+                if self.x > 0 - self.velocity:
+                    self.x -= self.velocity
+                    self.left = True
+                    self.right = False
+                    self.up = False
+                    self.down = False
+            elif keys[pygame.K_RIGHT]:
+                if self.x < 500 - self.width - self.velocity:
+                    self.x += self.velocity
+                    self.right = True
+                    self.left = False
+                    self.up = False
+                    self.down = False
+            elif keys[pygame.K_UP]:
+                if self.y > 0:
+                    self.y -= self.velocity
+                    self.right = False
+                    self.left = False
+                    self.up = True
+                    self.down = False
+            elif keys[pygame.K_DOWN]:
+                if self.y < 500 - self.height:
+                    self.y += self.velocity
+                    self.right = False
+                    self.left = False
+                    self.up = False
+                    self.down = True
+            else:
+                if self.right:
+                    if self.x < 500 - self.width - self.velocity:
+                        self.x += self.velocity
+                elif self.left:
+                    if self.x > 0 + self.velocity:
+                        self.x -= self.velocity
+                elif self.up:
+                    if self.y > 0 + self.velocity:
+                        self.y -= self.velocity
+                elif self.down:
+                    if self.y < 500 - self.height - self.velocity:
+                        self.y += self.velocity
+            print(self.x,self.y)
+            self.redraw()
 
 
 # if __name__ == "__main__":
