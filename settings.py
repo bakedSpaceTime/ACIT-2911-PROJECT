90b2cbebdd7b461bebe29a78070ba12ef3d74e6b
jeffreylaw <lawjeffrey35@gmail.com>
2020-04-27 16:53:48 -0700

    Revert "uploading"
    This reverts commit 04506524836014b3e3ce6035a6af67d39fe4d4ab, reversing
    changes made to b6c33f4ccfb5a7c62f6b518fb1144fcf9f758ab3.

--- settings.py
+++ settings.py
@@ -6,11 +6,3 @@ GAME_SETTINGS = {
     # window width
     "width": 500,
 }
-
-PLAYER_SETTINS = {
-    "sprite_height": 32,
-    "sprite_width": 32,
-    "starting_x": 225,
-    "starting_y": 300,
-    "velocity": 10,
-}
