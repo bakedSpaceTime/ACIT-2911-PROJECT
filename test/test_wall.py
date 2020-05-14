697ca497037d1e8bc5e234e9a89bb6a797a3f12a
myhsieh <mhsieh28@my.bcit.ca>
2020-05-14 08:41:37 -0700

    updated unit tests, and try to unit test on travis

--- test/test_wall.py
+++ test/test_wall.py
@@ -1,15 +1,15 @@
 import unittest
-from wall import Wall
+from wall import Obstacle
 
 
 class TestWall(unittest.TestCase):
     def setUp(self):
-        self.wall1 = Wall(0, 100, 450, 10)
+        self.wall1 = Obstacle(450, 10)
 
     def test_constructor(self):
         self.assertIsNotNone(self.wall1)
-        self.assertIsInstance(self.wall1, Wall)
+        self.assertIsInstance(self.wall1, Obstacle)
 
     def test_constructor_invalid(self):
         with self.assertRaises(TypeError):
-            wall2 = Wall('0', '4', 5, 7)
+            wall2 = Obstacle('0', '4', 5, 7)
