fbbd3da2531e4763f5eff64d04412890a019ed4d
bakedSpaceTime <tushyais@gmail.com>
2020-05-14 21:23:23 -0700

    add header docstring to files that need it

--- loot.py
+++ loot.py
@@ -1,3 +1,14 @@
+"""
+Pandemic Run
+Course: ACIT 2911, Agile Development
+Authors:
+- Jaskaran Saini, A01055847
+- Jeffery Law, A00864331
+- Ming Yen Hsieh, A01170219
+- Tushya Iyer, A01023434
+- Shivar Pillay, A01079978
+- Shivam Patel, A01185250
+"""
 import pygame
 from settings import OTHER_SPRITES
 WHITE = (255, 255, 255)
